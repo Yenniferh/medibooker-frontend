3048633d37e6c99cf65c85282dfc855b4a63e3cf
Yennifer Herrera <yennifer.herrera@ext.hellofresh.com>
2024-04-21 15:58:48 -0500

    UPD: Base styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "deep-teal": "#012E40",
+        "teal": "#026773",
+        "teal-gray": "#738187",
+        "action-red": "#C20F45",
+      },
+      fontFamily: {
+        headings: ['Raleway', "Helvetica", 'sans-serif'],
+        body: ['Montserrat', "Arial", 'sans-serif'],
+      },
+      screens: {
+        'xs': '420px',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+  ],
 };
